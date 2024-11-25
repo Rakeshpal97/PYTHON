fb554b19091889f8cd71b88c7287828e36662e68
RAKESH KUMAR <rakeshpal215622@gmail.com>
2024-11-25 18:27:59 +0530

    Add files via upload

---
+++ rakesh.py
@@ -0,0 +1,34 @@
+str1="this is my united university"
+str2= "i am a student"
+str=   "this is a string"
+str="this is a string,\n we are creating in python"
+print(str1) 
+str='apna school'
+ch=str[2]
+print(ch)
+str="united"
+str[1:4] is" nit"
+print(str[1:4])
+  # using string function:  
+str ="i am studing in united university"
+
+print(str.capitalize())
+str ="i am studing in united university"
+print(str.endswith("ity"))
+
+str = "rakesh pal"
+print(str.upper())
+str ="UNiTeD"
+print(str.lower())
+str ="rakeshpal at united university"
+print(len(str))
+name ="bca A CS"
+old ="bca A"
+new ="bca b"
+str1 =name.replace(old,new)
+print(name)
+print(str1)
+name =('aman','daman','saman')
+str1 =' '.join(name)
+print(name)
+print(str1)
